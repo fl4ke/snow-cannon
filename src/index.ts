6ab17001f4d02045616d3ff3d702335d61c002a0
fl4ke <fl4ke@tuta.io>
2019-12-25 23:14:34 +0100

    can now query for recent just.game interactions

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,18 @@ const program = require('commander');
 const ethers = require('ethers');
 const TronWeb = require('tronweb');
 const TronGrid = require('trongrid');
+const rp = require('request-promise');
+const contracts = require('../contracts.json');
 
-const CURVY_ADDR = "TWjkoz18Y48SgWoxEeGG11ezCCzee8wo1A";
-const VANITY_CONTRACT = "TV7DiSukGoP1h5KswfWVk9LWgED6sSxTt8";
 const TRON_NODE = "https://api.trongrid.io";
 const SOLIDITY_NODE = "https://api.trongrid.io";
+
+const CURVY_INTERFACE = new ethers.utils.Interface(contracts.curvy.abi);
+const VANITY_INTERFACE = new ethers.utils.Interface(contracts.vanity.abi);
+
+const CONTRACTS = new Map();
+CONTRACTS.set(contracts.curvy.contract, CURVY_INTERFACE);
+CONTRACTS.set(contracts.vanity.contract, VANITY_INTERFACE);
 
 clear();
 console.log(
@@ -25,6 +32,7 @@ program
     .description("Command line tool to troubleshoot curvy")
     .option('-r, --round', 'Get Current round info')
     .option('-i, --info <address>', 'Get player stats')
+    .option('-t, --transactions <address>', 'Get recen just.game transactions')
     .parse(process.argv);
 
 
@@ -34,7 +42,7 @@ if (!process.argv.slice(2).length) {
 
 const tronWeb = new TronWeb(TRON_NODE, SOLIDITY_NODE);
 // Using curvy's address as the fallback address for the request
-tronWeb.setAddress(CURVY_ADDR);
+tronWeb.setAddress(contracts.curvy.address);
 const tronGrid = new TronGrid(tronWeb);
 
 const main = async () => {
@@ -53,6 +61,17 @@ const main = async () => {
             console.error(e);
         }
     }
+    if (program.transactions) {
+        try {
+            await displayRecentTransactions(program.transactions);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+};
+
+const formatNumber = (n: number): string => {
+    return n.toLocaleString();
 };
 
 const displayBalances = async (userAddress: string) => {
@@ -60,7 +79,7 @@ const displayBalances = async (userAddress: string) => {
 
     const contract = await tronWeb
         .contract()
-        .at(CURVY_ADDR);
+        .at(contracts.curvy.address);
 
     const userBalance = await tronWeb.trx.getBalance(userAddress);
     console.log(`Players wallet balance: ${ formatNumber(userBalance / 1000000) } trx`);
@@ -68,12 +87,11 @@ const displayBalances = async (userAddress: string) => {
     try {
         const contract = await tronWeb
             .contract()
-            .at(VANITY_CONTRACT);
+            .at(contracts.vanity.address);
 
         const vanity = await resolveRefName(contract, userAddress);
-        console.log(`Players Vanity Name: ${vanity}`);
+        console.log(`Players Vanity Name: ${ vanity }`);
     } catch (e) {
-        console.log(e);
     }
 
     const currentRoundNumber = await getCurrentRoundNumber(contract);
@@ -96,7 +114,7 @@ const displayBalances = async (userAddress: string) => {
 const displayCurrentRoundInfo = async () => {
     const contract = await tronWeb
         .contract()
-        .at(CURVY_ADDR);
+        .at(contracts.curvy.address);
 
     const currentRoundData = await getCurrentRoundInfoData(contract);
     console.log(`currentRoundData:
@@ -113,6 +131,19 @@ const displayCurrentRoundInfo = async () => {
      totalTransactionCount: ${ formatNumber(currentRoundData.totalTransactionCount) }
      roundNumber: ${ formatNumber(currentRoundData.roundNumber) }
      totalTronPledged: ${ formatNumber(currentRoundData.totalTronPledged / 1000000) }`);
+};
+
+const displayRecentTransactions = async (address: string) => {
+    const transactions = await getRecentTransactionData(address);
+    if (transactions) {
+        console.log(`Recent transactions:`);
+        for (const tx of transactions) {
+            console.log(`  ${ tx.status } ${ tx.value } trx ${ tx.operation }`);
+            for (const input of tx.inputs) {
+                console.log(`    ${ input.name } ${ input.value }`);
+            }
+        }
+    }
 };
 
 const getPlayerInfo = async (contract: any, userAddress: string) => {
@@ -136,13 +167,62 @@ const getCurrentRoundInfoData = async (contract: any) => {
     return await contract.currentRoundData().call();
 };
 
+const getRecentTransactionData = async (userAddress: string) => {
+    let data = [];
+    const options = {
+        method: 'GET',
+        uri: `https://api.trongrid.io/v1/accounts/${ userAddress }/transactions?only_from=true&limit=200`,
+        json: true,
+    };
+    const resp = await rp(options);
+    if (resp.success) {
+        for (const tx of resp.data) {
+            if (tx.raw_data.contract[0].type === "TriggerSmartContract") {
+                const contractAddress = tx.raw_data.contract[0].parameter.value.contract_address.slice(2);
+                const abiInterface = CONTRACTS.get(contractAddress);
+                if (abiInterface) {
+                    const decodedTx = abiInterface.parseTransaction({
+                        data: "0x" + tx.raw_data.contract[0].parameter.value.data
+                    });
+                    const funcAbi = abiInterface.functions[decodedTx.name];
+                    let callValue = tx.raw_data.contract[0].parameter.value.call_value;
+                    if (!callValue) {
+                        callValue = 0;
+                    }
+                    data.push({
+                        status: tx.ret[0].code,
+                        value: formatNumber(callValue / 1000000),
+                        operation: decodedTx.name,
+                        inputs: parseArguments(funcAbi.inputs, decodedTx.args),
+                    });
+                }
+            }
+        }
+        return data;
+    }
+}
+
 const resolveRefName = async (contract: any, userAddress: string) => {
     const resp = await contract.resolveToName(userAddress).call();
     return ethers.utils.parseBytes32String(resp);
 };
 
-const formatNumber = (n: number): string => {
-    return n.toLocaleString();
+const parseArguments = (inputs: any, args: any) => {
+    let data = [];
+    for (let i = 0; i < inputs.length; i++) {
+        let input = {
+            name: i,
+            value: args[i].toString(),
+        };
+        if (inputs[i].name) {
+            input.name = inputs[i].name;
+        }
+        if (inputs[i].type === 'bytes32') {
+            input.value = ethers.utils.parseBytes32String(args[i]);
+        }
+        data.push(input);
+    }
+    return data;
 };
 
 main();
